86fa258c69c188e0faf5932ec169dd9cd417df5e
AmandaSuryani <amanda.berliana.2203226@students.um.ac.id>
2024-09-09 10:51:04 +0700

    Update StudyToday.py

--- StudyToday.py
+++ StudyToday.py
@@ -1,5 +1,5 @@
 import streamlit as st 
-st.Fisika
-st. Komputasi
-st.  Awan
-st ('Amanda Berliana Indah Suryani / 220322608247')
+st.title("Fisika')
+st.title("Komputasi")
+st.title("Awan")
+st ("Amanda Berliana Indah Suryani / 220322608247")
